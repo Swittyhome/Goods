68c4e69a68da58eb7d7b5fe8b07f008e3027ffd8
Switlana22 <switty1979@gmail.com>
2019-06-19 15:24:48 +0300

    Hello

---
+++ Class_Goods.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ConsoleApplication80
+{
+     class Goods
+    {
+        int ID_goods { get; set; }
+        string name;
+        int category;
+        string production_date;
+        string expiration_date;
+        int price_per_unit { get; set; }
+        string brief_description;
+        string notes;
+
+          }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Goods goods = new Goods();
+            Console.WriteLine();
+            Console.ReadKey();
+
+        }
+    }
+}
